cac0bc32e71cf305dca7bb557d949d8b981c5eaf
David Pick <david@david-laptop.(none)>
2010-08-13 15:57:36 -0500

    renamed and added a view

--- app.rb
+++ app.rb
@@ -10,14 +10,19 @@ configure do
   $player = MusicPlayer.launched or abort "Error: no music player launched!"
 end
 
+get '/' do
+  haml :index 
+end
+
+get '/music' do
+  @title = $player.current_track
+  @artist = $player.current_artist
+  @album = $player.current_album
+  haml :music
+end
+
 post '/player' do
   params.each { |k, v| $player.send(k) if $player.respond_to?(k) }
   redirect '/'
 end
 
-get '/' do
-  @title = $player.current_track
-  @artist = $player.current_artist
-  @album = $player.current_album
-  haml :index
-end
