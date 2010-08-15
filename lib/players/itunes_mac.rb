b21c75109be51a5e75488e85f3bbdd0b7743663f
Derek Hammer <derek.hammer@thoughtworks.com>
2010-08-15 15:08:58 -0500

    Using Name to remove duplication

--- lib/players/itunes_mac.rb
+++ lib/players/itunes_mac.rb
@@ -28,7 +28,7 @@ class ItunesPlayer_MacOSX < MusicPlayer
   end
 
   def launched?
-    %x(osascript -e 'tell app "System Events" to count (every process whose name is "iTunes")')
+    %x(osascript -e 'tell app "System Events" to count (every process whose name is "#{name}")')
   end
 
   def name
@@ -37,6 +37,6 @@ class ItunesPlayer_MacOSX < MusicPlayer
 
   private
   def tell_to(command)
-    %x(osascript -e 'tell app "iTunes" to #{command}')
+    %x(osascript -e 'tell app "#{name}" to #{command}')
   end
 end
